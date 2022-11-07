115a462f00237300d9b0f2cee67682844176953e
RaulMartino <raulmartino@hotmail.com>
2022-11-06 22:11:11 -0300

    Adicionei as configurações de usuário

---
+++ frontend/src/components/description.js
@@ -0,0 +1,12 @@
+function description(){
+    return(
+        <form>
+            <div>
+            <input type="text" placeholder="Insira uma descrição sobre você mesmo"/> 
+            </div>
+            <input type="submit" value="Salvar" />
+        </form>
+    )
+}
+
+export default description
--- frontend/src/routes/usersettings.jsx
+++ frontend/src/routes/usersettings.jsx
@@ -1,10 +1,32 @@
 import Header from "../components/header";
 import image from "../images/userpicture.png";
 
+import "./usersettings.css";
+
 export default function Screen() {
   return (
     <>
       <Header />
+      <div id="settings-area">
+        <div className="container">
+          <div className="mt-5 mx-3">
+              <h2 className="text-light">Edit your profile:</h2>
+              <form action="">
+                <div class="form-group">
+                  <label>Username:</label>
+                  <input type="text" class="form-control" placeholder="Change username"></input>
+                </div>
+                <br></br>
+                <div class="form-group">
+                  <label>Bio:</label>
+                  <input type="text" class="form-control" placeholder="Change bio"></input>
+                </div>
+                <br></br>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form> 
+          </div>
+        </div>
+      </div>
     </>
   );
 }
